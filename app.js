87510390a78960b3a361b2359b422a72ba37564a
SerePate <serepate@umail.iu.edu>
2017-06-29 19:38:57 -0400

    updated site from class. Day 4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = {
     this.flicks = []
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
 
     document
       .querySelector(selectors.formSelector)
@@ -12,37 +13,50 @@ const app = {
       )
   },
 
-  toFavorite(flick,ev){
+  favFlick(flick, ev) {
     const listItem = ev.target.closest('.flick')
     flick.fav = !flick.fav
 
-    if(flick.fav){
+    if (flick.fav) {
       listItem.classList.add('fav')
-    }
-    else{
+    } else {
       listItem.classList.remove('fav')
     }
   },
 
+  removeFlick(flick, ev) {
+    // remove from the DOM
+    const listItem = ev.target.closest('.flick')
+    listItem.remove()
+
+    // remove from the array
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
+  },
 
   renderListItem(flick) {
-    const item = document.createElement('div')
-    item.dataset.id=flick.id
-    
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
     item
       .querySelector('.flick-name')
       .textContent = flick.name
 
     item
-      .querySelector("button.fav")
+      .querySelector('button.remove')
       .addEventListener(
-        'click',
-        this.toFavorite.bind(this,flick)
+        'click', 
+        this.removeFlick.bind(this, flick)
       )
 
+    item
+      .querySelector('button.fav')
+      .addEventListener(
+        'click', 
+        this.favFlick.bind(this, flick)
+      )
+    
     return item
-
-
   },
 
   handleSubmit(ev) {
@@ -51,29 +65,22 @@ const app = {
     const flick = {
       id: this.max + 1,
       name: f.flickName.value,
+      fav: false,
     }
 
+    this.flicks.unshift(flick)
+
     const listItem = this.renderListItem(flick)
-    this.list.appendChild(listItem)
+    this.list
+      .insertBefore(listItem, this.list.firstElementChild)
 
-    this.flicks.push(flick.name)
-  
     this.max ++
-    
-    
+    f.reset()
   },
-  
-  highlight(ev,flicks){ //when clicked, add a background color to the text
-     ev.preventDefault()
-     
-
-   },
-
-
- }
-
+}
 
 app.init({
   formSelector: 'form#flick-form',
   listSelector: '#flick-list',
+  templateSelector: '.flick.template',
 })
